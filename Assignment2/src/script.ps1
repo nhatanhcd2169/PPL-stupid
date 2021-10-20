6565b4f47fa640d7177d8271ee91e753885b8642
Nguyễn Nhật Anh <55342178+nhatanhcd2169@users.noreply.github.com>
2021-10-20 14:25:05 +0700

    Update

---
+++ Assignment2/src/script.ps1
@@ -0,0 +1,9 @@
+cls
+"=============================ParserSuite=============================="
+
+python run.py gen
+python run.py test ParserSuite
+""
+""
+"=============================ASTGenSuite=============================="
+python run.py test ASTGenSuite
